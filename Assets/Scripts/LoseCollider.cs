4153cab8352b98f163f207d7609a7e95c7d8f2f2
kurtnovack <kurt@walltowall.com>
2018-03-17 01:19:30 -0400

    Adds start and win screen / adds ball and trigger

---
+++ Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoseCollider : MonoBehaviour {
+
+	public LevelManager levelManager;
+
+	private void OnTriggerEnter2D(Collider2D collision) {
+		print("Trigger");
+		levelManager.LoadLevel("Win");
+	}
+}
